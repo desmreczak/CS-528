1308c6cc88fe3774e256db79ced7a26c2004fa6f
nik <ngcheung@wpi.edu>
2017-10-03 00:39:59 -0400

    cosmetic changes

--- 16_CameraIntent/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ 16_CameraIntent/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -96,7 +96,7 @@ public class CrimeListFragment extends Fragment {
 
     private void updateSubtitle() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
-        int crimeCount = crimeLab.getCrimes().size();
+        String crimeCount = Integer.toString(crimeLab.getCrimes().size());
         String subtitle = getString(R.string.subtitle_format, crimeCount);
 
         if (!mSubtitleVisible) {
